ae665bc240fa8e53041b2bccceff2b74469ed6a8
Han Tuzun <mail@hantuzun.com>
2018-08-16 16:48:31 +0300

    Remove -q statements from wget

--- install_compiler.sh
+++ install_compiler.sh
@@ -5,8 +5,8 @@ DEST=/opt/bolos
 mkdir -p ${DEST}
 cd ${DEST}
 
-wget -q https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q1-update/+download/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2
-wget -q http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q1-update/+download/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2
+wget http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
 
 tar xfj gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2
 tar xfv clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
@@ -21,7 +21,7 @@ ln -s ${DEST}/clang-arm-fropi/bin/clang /usr/bin/clang
 # Avoid high UID/GID that affect CircleCI
 chown root:root /opt/bolos -R
 
-#wget -q -O - https://www.ledgerwallet.com/support/add_udev_rules.sh | bash
+#wget -O - https://www.ledgerwallet.com/support/add_udev_rules.sh | bash
 #usermod -a -G plugdev vagrant
 
 echo "export BOLOS_ENV=/opt/bolos" >> ~/.bashrc
